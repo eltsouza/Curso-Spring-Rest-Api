5cb43c46103f2d73df9562cded157caeb198e107
eltsouza <tonton_elton@hotmail.com>
2021-01-11 11:43:20 -0300

    Relacionamento ManyToOne

---
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -0,0 +1,27 @@
+package curso.api.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/index")
+public class IndexController {
+
+	/*Serviço RESTful*/
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity init(@RequestParam(value = "nome", required = false, defaultValue = "Nome não informado") String nome) {
+		
+		System.out.println("Parametro recebido.:"+ nome);
+		
+		return new ResponseEntity("Olá Usuário REST Spring Boot: Seu nome é :" + nome , HttpStatus.OK);
+	}
+
+	
+	
+	
+	
+}
---
+++ src/main/java/curso/api/rest/model/Telefone.java
@@ -0,0 +1,20 @@
+package curso.api.rest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Telefone {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String numero;
+
+	@ManyToOne
+	private Usuario usuario;
+}
