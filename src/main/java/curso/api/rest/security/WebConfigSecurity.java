c7f597bc7df0f853de64a38ee8e9bae70e32e777
eltsouza <tonton_elton@hotmail.com>
2021-02-17 15:58:54 -0300

    Implementando a Classe WebConfigSecurity

---
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -0,0 +1,62 @@
+package curso.api.rest.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import curso.api.rest.service.ImplementacaoUserDetailsService;
+
+/*mapeia,URL, endereços, autoriza ou bloqueio acessos a URL*/
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+    private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+	
+	/*configura as solicitacoes de acessos por HTTP*/
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+    
+		/*ativando a protecao contra usuario que não estao validados por token*/
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		
+		/*Ativando a permissão para acesso a página inicial do sistema. Ex. sistema.com.br/index*/
+		.disable().authorizeRequests().antMatchers("/").permitAll()
+		.antMatchers("/index").permitAll()
+		
+		/*URL Logout - redireciona apos o usuario desologar do sistema*/
+		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+		
+		/*Mapeia a URL e Logout e invalida o usuario*/
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		
+		/*Filtra as requisicoes de login para autenticacao*/
+		
+		
+		/*Filtra demais requisicoes para verificar a presenca do TOKEN JWT no HEADER HTTP*/
+		
+		
+		
+	}
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+   	
+	   /* Service que irá consultar o usuário no banco de dados */	
+	   auth.userDetailsService(implementacaoUserDetailsService)
+	   /*padrao de codificacao de senha do usuario*/
+	   .passwordEncoder(new BCryptPasswordEncoder());
+	
+	}
+	
+
+}
