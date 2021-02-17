92e7114a9931e672e925501fcc40e94e8a5d2781
eltsouza <tonton_elton@hotmail.com>
2021-02-17 18:30:04 -0300

    Criando a classe JwtApiAutenticacaoFilter

--- src/main/java/curso/api/rest/security/WebConfigSecurity.java
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package curso.api.rest.security;
 
+import org.aspectj.weaver.ast.And;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -36,13 +38,14 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
 		
 		/*Mapeia a URL e Logout e invalida o usuario*/
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 		
 		/*Filtra as requisicoes de login para autenticacao*/
-		
+		.and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+				UsernamePasswordAuthenticationFilter.class)
 		
 		/*Filtra demais requisicoes para verificar a presenca do TOKEN JWT no HEADER HTTP*/
-		
+		.addFilterBefore(new JWTApiAutenticacaoFilter(),UsernamePasswordAuthenticationFilter.class);
 		
 		
 	}
