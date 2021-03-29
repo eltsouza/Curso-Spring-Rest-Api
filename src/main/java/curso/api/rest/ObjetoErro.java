534b0060f92a372fd2d5688ced79f55fb726e958
eltsouza <tonton_elton@hotmail.com>
2021-03-29 20:06:14 -0300

    Controle de erros da API com @ControllerAdvice, @RestControllerAdvice e @ExceptionHandler

---
+++ src/main/java/curso/api/rest/ObjetoErro.java
@@ -0,0 +1,27 @@
+package curso.api.rest;
+
+
+public class ObjetoErro {
+	
+	
+	private String error;
+	private String code;
+	
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+	public String getError() {
+		return error;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+
+}
